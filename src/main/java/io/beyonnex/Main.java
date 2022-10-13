d8200d7474187bc41f3d8e919842454aebdc6d31
Rafael Zanella <rzanella@tripadvisor.com>
2022-10-13 14:57:22 +0100

    Added basic CLI-running option to application

--- src/main/java/io/beyonnex/Main.java
+++ src/main/java/io/beyonnex/Main.java
@@ -2,6 +2,20 @@ package io.beyonnex;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 2) {
+            System.out.println("Two words are needed");
+
+            System.exit(1);
+        }
+
+        System.out.println(
+            String.join(
+                " ",
+                args[1],
+                (AnagramChecker.isAnagram(args[0], args[1]) ? "is" : "isn't"),
+                "an anagram of",
+                args[0]
+            )
+        );
     }
 }
